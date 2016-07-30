558d3015e74372fe39db254be8290f1f69ed6017
Pablo <pablofsilva91@gmail.com>
2016-07-29 21:32:05 -0300

    Inclusion of GenericDomain.java file

---
+++ src/main/java/br/com/sisvendas/domain/GenericDomain.java
@@ -0,0 +1,26 @@
+package br.com.sisvendas.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass // será usada por outros para gerar tabelas
+public class GenericDomain implements Serializable{
+	@Id // chave primária
+	@GeneratedValue(strategy = GenerationType.AUTO) // chave criada pelo banco
+	private Long codigo; 
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+}
