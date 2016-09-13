0109ffc73d03e65c70cb2e684ca38b934952e39a
pablo.silvaf <pablo.silvaf@100602400-21933.ctis.local>
2016-09-13 14:40:36 -0300

    Update file

--- src/main/java/br/com/sisvendas/bean/EstadoBean.java
+++ src/main/java/br/com/sisvendas/bean/EstadoBean.java
@@ -1,7 +1,9 @@
 package br.com.sisvendas.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -15,12 +17,30 @@ import br.com.sisvendas.domain.Estado;
 @ViewScoped
 public class EstadoBean implements Serializable {
 	private Estado estado;
+	private List<Estado> estados;
 	
 	public Estado getEstado() {
 		return estado;
 	}
 	public void setEstado(Estado estado) {
 		this.estado = estado;
+	}
+	public List<Estado> getEstados() {
+		return estados;
+	}
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+	
+	@PostConstruct//depois do managed bean criado ele chama automaticamente o metodo listar
+	public void listar(){
+		try{
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		}catch(RuntimeException erro){
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar o Estado!");
+			erro.printStackTrace();
+		}
 	}
 	
 	public void novo(){
