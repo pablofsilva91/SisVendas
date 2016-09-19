fd45953619911d6da8dc4e55072b1fe9dd821e4c
pablo.silvaf <pablo.silvaf@100602400-21933.ctis.local>
2016-09-19 14:01:15 -0300

    Creation of method save

--- src/main/java/br/com/sisvendas/bean/CidadeBean.java
+++ src/main/java/br/com/sisvendas/bean/CidadeBean.java
@@ -10,7 +10,9 @@ import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
 import br.com.sisvendas.dao.CidadeDAO;
+import br.com.sisvendas.dao.EstadoDAO;
 import br.com.sisvendas.domain.Cidade;
+import br.com.sisvendas.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -19,6 +21,7 @@ import br.com.sisvendas.domain.Cidade;
 public class CidadeBean implements Serializable {
 	private Cidade cidade;
 	private List<Cidade> cidades;
+	private List<Estado> estados;
 	
 	public List<Cidade> getCidades(){
 		return cidades;
@@ -35,6 +38,14 @@ public class CidadeBean implements Serializable {
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
+	
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
 
 	@PostConstruct
 	public void listar(){
@@ -47,8 +58,30 @@ public class CidadeBean implements Serializable {
 	}
 	
 	public void novo(){
-		cidade = new Cidade();
+		try{
+			cidade = new Cidade();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();	
+			estados = estadoDAO.listar();
+		}catch(RuntimeException erro){
+			Messages.addFlashGlobalError("Ocorreu um erro ao gerar uma nova cidades");
+		}
 	}
 	
+	public void salvar(){
+		try{
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.merge(cidade);
+			
+			cidade = new Cidade();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			cidades = cidadeDAO.listar();
+			
+		}catch(RuntimeException erro){
+			Messages.addFlashGlobalError("Ocorreu um erro ao listar as cidades");
+		}
+	}
 
 }
