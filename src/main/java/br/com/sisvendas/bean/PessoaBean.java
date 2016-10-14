b99e96f2a43bc2690021413704ee0ad20d045b0f
pablo.silvaf <pablo.silvaf@100602400-21933.ctis.local>
2016-10-14 14:16:11 -0300

    Including method save

--- src/main/java/br/com/sisvendas/bean/PessoaBean.java
+++ src/main/java/br/com/sisvendas/bean/PessoaBean.java
@@ -1,6 +1,7 @@
 package br.com.sisvendas.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,7 +10,11 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.sisvendas.dao.CidadeDAO;
+import br.com.sisvendas.dao.EstadoDAO;
 import br.com.sisvendas.dao.PessoaDAO;
+import br.com.sisvendas.domain.Cidade;
+import br.com.sisvendas.domain.Estado;
 import br.com.sisvendas.domain.Pessoa;
 
 @ViewScoped
@@ -18,6 +23,8 @@ import br.com.sisvendas.domain.Pessoa;
 public class PessoaBean implements Serializable {
 	private Pessoa pessoa;
 	private List<Pessoa> pessoas;
+	private List<Cidade> cidades;
+	private List<Estado> estados;
 	
 	public Pessoa getPessoa() {
 		return pessoa;
@@ -32,6 +39,18 @@ public class PessoaBean implements Serializable {
 		this.pessoas = pessoas;
 	}
 	
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+	public List<Estado> getEstados() {
+		return estados;
+	}
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
 	@PostConstruct
 	public void listar(){
 		try{
@@ -45,7 +64,35 @@ public class PessoaBean implements Serializable {
 	}
 	
 	public void novo(){
+		try{
+			pessoa = new Pessoa();
 		
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		
+			cidades = new ArrayList<Cidade>();//lista vazia
+		}catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao salvar a Pessoa");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void salvar(){
+//		try{
+//			PessoaDAO pessoaDAO = new PessoaDAO();
+//			pessoaDAO.merge(pessoa);
+//			
+//			pessoa = new Pessoa();
+//			
+//			EstadoDAO estadoDAO = new EstadoDAO();
+//			estados = estadoDAO.listar();
+//			CidadeDAO cidadeDAO = new CidadeDAO();
+//			cidades = cidadeDAO.listar();
+//			pessoas = pessoaDAO.listar();
+//			
+//		}catch(RuntimeException erro){
+//			Messages.addFlashGlobalError("Ocorreu um erro ao listar as cidades");
+//		}
 	}
 	
 	public void excluir(){
